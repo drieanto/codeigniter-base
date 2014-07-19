e8df99f952fd5bda1fc31bedf25ee13292ec002f
Edwin Andrianto <drieanto@live.com>
2014-07-19 23:20:16 +0700

    first init

---
+++ application/logs/log-2014-07-19.php
@@ -0,0 +1,19 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-07-19 18:11:36 --> Config Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Hooks Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Utf8 Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> UTF-8 Support Enabled
+DEBUG - 2014-07-19 18:11:36 --> URI Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Router Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> No URI present. Default controller set.
+DEBUG - 2014-07-19 18:11:36 --> Output Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Security Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Input Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-07-19 18:11:36 --> Language Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Loader Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> Controller Class Initialized
+DEBUG - 2014-07-19 18:11:36 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2014-07-19 18:11:36 --> Final output sent to browser
+DEBUG - 2014-07-19 18:11:36 --> Total execution time: 0.0470
